05f24f626eda12382395f3f4b8f7f20af579f243
Sebastian Ludwig <sebastian@lurado.de>
2015-10-31 21:16:57 +0100

    Removed now obsolete test_generate_string_file_1.

--- test/test_twine.rb
+++ test/test_twine.rb
@@ -117,14 +117,6 @@ end
 
 class TestTwine < TwineTestCase
 
-  def test_generate_string_file_1
-    Dir.mktmpdir do |dir|
-      output_path = File.join(dir, 'fr.xml')
-      Twine::Runner.run(%W(generate-string-file test/fixtures/strings-1.txt #{output_path}))
-      assert_equal(ERB.new(File.read('test/fixtures/test-output-1.txt')).result, File.read(output_path))
-    end
-  end
-
   def test_generate_string_file_2
     Dir.mktmpdir do |dir|
       output_path = File.join(dir, 'en.strings')
