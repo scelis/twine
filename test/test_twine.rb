83aa0175d6a46e1ae6b713225d2736c70b09d14b
Sebastian Ludwig <sebastian@lurado.de>
2015-11-01 22:18:00 +0100

    Removed now obsolete test_generate_string_file_3.

--- test/test_twine.rb
+++ test/test_twine.rb
@@ -2,16 +2,6 @@ require 'twine_test_case'
 
 class TestTwine < TwineTestCase
 
-  def test_generate_string_file_3
-    Dir.mktmpdir do |dir|
-      # jQuery formatter recognition
-      # jQuery formatter
-      output_path = File.join(dir, 'en.json')
-      Twine::Runner.run(%W(generate-string-file test/fixtures/strings-1.txt #{output_path} -t tag1))
-      assert_equal(ERB.new(File.read('test/fixtures/test-output-5.txt')).result, File.read(output_path))
-    end
-  end
-
   def test_generate_string_file_4
     Dir.mktmpdir do |dir|
       # key with space
