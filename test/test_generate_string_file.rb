9cc3649e7da78628b601e0121feb3331d6d967c7
Sebastian Ludwig <sebastian@lurado.de>
2015-11-01 22:17:40 +0100

    Extracted jQuery formatter deduction test from test_generate_string_file_3.

--- test/test_generate_string_file.rb
+++ test/test_generate_string_file.rb
@@ -40,6 +40,20 @@ class TestGenerateStringFile < TwineTestCase
     runner.generate_string_file
   end
 
+  def test_deducts_jquery_format_from_output_path
+    mock_jquery_formatter = Twine::Formatters::Apple.new(@mock_strings, {})
+    Twine::Formatters::JQuery.stubs(:new).returns(mock_jquery_formatter)
+    options = {
+      output_path: File.join(@output_dir, 'fr.json'),
+      languages: ['fr']
+    }
+    runner = Twine::Runner.new(nil, options)
+
+    mock_jquery_formatter.expects(:write_file)
+
+    runner.generate_string_file
+  end
+
   def test_deducts_language_from_output_path
     random_language = @known_languages.sample
     options = {
