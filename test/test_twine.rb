a19be4d4f010fad621c6e5a5094176495b97e1e8
Sebastian Ludwig <sebastian@lurado.de>
2015-11-01 22:04:10 +0100

    Removed now obsolete test test_generate_string_file_2.

--- test/test_twine.rb
+++ test/test_twine.rb
@@ -45,17 +45,6 @@ end
 
 class TestTwine < TwineTestCase
 
-  def test_generate_string_file_2
-    Dir.mktmpdir do |dir|
-      # Aspects:
-      #  - Apple formatter
-      #  - Tags
-      output_path = File.join(dir, 'en.strings')
-      Twine::Runner.run(%W(generate-string-file test/fixtures/strings-1.txt #{output_path} -t tag1))
-      assert_equal(ERB.new(File.read('test/fixtures/test-output-2.txt')).result, File.read(output_path))
-    end
-  end
-
   def test_generate_string_file_3
     Dir.mktmpdir do |dir|
       # jQuery formatter recognition
