fcc6508779361a5bbf352b3839d9f8a77fd56b59
Sebastian Ludwig <sebastian@lurado.de>
2015-11-02 10:22:38 +0100

    Added support for multiple translations to TwineFileDSL.

--- test/twine_file_dsl.rb
+++ test/twine_file_dsl.rb
@@ -20,11 +20,16 @@ module TwineFileDSL
     return unless @currently_built_twine_file
     return unless @currently_built_twine_file_section
 
-    language = parameters[:language] || @currently_built_twine_file.language_codes.first
-
     # this relies on Ruby 1.9 preserving the order of hash elements
     row = Twine::StringsRow.new(parameters.first[0].to_s)
-    row.translations[language] = parameters.first[1]
+    if parameters.first[1].is_a? Hash
+      parameters.first[1].each do |language, translation|
+        row.translations[language.to_s] = translation
+      end
+    else
+      language = @currently_built_twine_file.language_codes.first
+      row.translations[language] = parameters.first[1]
+    end
     row.comment = parameters[:comment] if parameters[:comment]
     row.tags = parameters[:tags] if parameters[:tags]
 
