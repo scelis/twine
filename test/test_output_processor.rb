1b05722178a7d1153491941d8e8488b40e4cb568
Sebastian Ludwig <sebastian@lurado.de>
2015-11-02 10:26:49 +0100

    Added test_include_translated TestOutputProcessor to replace test_generate_string_file_6.

--- test/test_output_processor.rb
+++ test/test_output_processor.rb
@@ -4,17 +4,17 @@ class TestOutputProcessor < TwineTestCase
   def setup
     super
 
-    @strings = build_twine_file 'en' do
+    @strings = build_twine_file 'en', 'fr' do
       add_section 'Section 1' do
         add_row key1: 'value1', tags: ['tag1']
         add_row key2: 'value2', tags: ['tag1', 'tag2']
         add_row key3: 'value3', tags: ['tag2']
-        add_row key4: 'value4'
+        add_row key4: { en: 'value4-en', fr: 'value4-fr' }
       end
     end
   end
 
-  def test_includes_all_tags_by_default
+  def test_includes_all_keys_by_default
     processor = Twine::Processors::OutputProcessor.new(@strings, {})
     result = processor.process('en')
 
@@ -40,5 +40,12 @@ class TestOutputProcessor < TwineTestCase
     result = processor.process('en')
 
     assert_equal %w(key1 key2 key4), result.strings_map.keys.sort
+  end
+
+  def test_include_translated
+    processor = Twine::Processors::OutputProcessor.new(@strings, { include: 'translated' })
+    result = processor.process('fr')
+
+    assert_equal %w(key4), result.strings_map.keys.sort
   end
 end
