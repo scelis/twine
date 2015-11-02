f424d7121087662013f964e05b81f5469e4f4719
Sebastian Ludwig <sebastian@lurado.de>
2015-11-02 10:21:44 +0100

    Corrected expected/actual order of assertions in TestOuputProcessor.

--- test/test_output_processor.rb
+++ test/test_output_processor.rb
@@ -18,27 +18,27 @@ class TestOutputProcessor < TwineTestCase
     processor = Twine::Processors::OutputProcessor.new(@strings, {})
     result = processor.process('en')
 
-    assert_equal result.strings_map.keys.sort, %w(key1 key2 key3 key4)
+    assert_equal %w(key1 key2 key3 key4), result.strings_map.keys.sort
   end
 
   def test_filter_by_tag
     processor = Twine::Processors::OutputProcessor.new(@strings, { tags: ['tag1'] })
     result = processor.process('en')
 
-    assert_equal result.strings_map.keys.sort, %w(key1 key2)
+    assert_equal %w(key1 key2), result.strings_map.keys.sort
   end
 
   def test_filter_by_multiple_tags
     processor = Twine::Processors::OutputProcessor.new(@strings, { tags: ['tag1', 'tag2'] })
     result = processor.process('en')
 
-    assert_equal result.strings_map.keys.sort, %w(key1 key2 key3)
+    assert_equal %w(key1 key2 key3), result.strings_map.keys.sort
   end
 
   def test_filter_untagged
     processor = Twine::Processors::OutputProcessor.new(@strings, { tags: ['tag1'], untagged: true })
     result = processor.process('en')
 
-    assert_equal result.strings_map.keys.sort, %w(key1 key2 key4)
+    assert_equal %w(key1 key2 key4), result.strings_map.keys.sort
   end
 end
