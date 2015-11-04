4a60ce3c467edd5686fe9003c4ed6f3f0268cf47
Sebastian Ludwig <sebastian@lurado.de>
2015-11-04 12:51:27 +0100

    Removed now obsolete test_consume_string_file_5.

--- test/test_twine.rb
+++ test/test_twine.rb
@@ -11,15 +11,6 @@ require 'twine_test_case'
 # fallback to developer language
 
 class TestTwine < TwineTestCase
-  def test_consume_string_file_5
-    Dir.mktmpdir do |dir|
-      # multi line string parsing
-      output_path = File.join(dir, 'strings.txt')
-      Twine::Runner.run(%W(consume-string-file test/fixtures/strings-1.txt test/fixtures/en-2.po -o #{output_path} -l en -a))
-      assert_equal(File.read('test/fixtures/test-output-9.txt'), File.read(output_path))
-    end
-  end
-
   def test_generate_string_file_14_references
     Dir.mktmpdir do |dir|
       output_path = File.join(dir, 'references.xml')
