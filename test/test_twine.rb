6b2c364e334d6704e23a5deab93f5ec42cc96b14
Sebastian Ludwig <sebastian@lurado.de>
2015-11-04 18:43:45 +0100

    Removed now obsolete test_generate_string_file_14_references (last test from test_twine - yay!)

--- test/test_twine.rb
+++ test/test_twine.rb
@@ -9,13 +9,3 @@ require 'twine_test_case'
 #     add accents around values with trailing space
 #     add accents around values wrapped in accents
 # fallback to developer language
-
-class TestTwine < TwineTestCase
-  def test_generate_string_file_14_references
-    Dir.mktmpdir do |dir|
-      output_path = File.join(dir, 'references.xml')
-      Twine::Runner.run(%W(generate-string-file test/fixtures/strings-4-references.txt #{output_path} -l fr -t tag1))
-      assert_equal(ERB.new(File.read('test/fixtures/test-output-14-references.txt')).result, File.read(output_path))
-    end
-  end  
-end
