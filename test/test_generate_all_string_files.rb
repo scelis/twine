17d206f7ce7beef5ad01eb520bd4e50d9e2c0ae5
Sebastian Ludwig <sebastian@lurado.de>
2015-11-28 01:18:39 +0100

    Changed TestCrateFolders to be a CommandTestCase and renamed it to TestGenerateAllStringFiles.

---
+++ test/test_generate_all_string_files.rb
@@ -0,0 +1,44 @@
+require 'command_test_case'
+
+class TestGenerateAllStringFiles < CommandTestCase
+  class TestCreateFolders < CommandTestCase
+    def new_runner(create_folders)
+      options = {}
+      options[:output_path] = @output_dir
+      options[:format] = 'apple'
+      options[:create_folders] = create_folders
+
+      @twine_file = build_twine_file 'en', 'es' do
+        add_section 'Section' do
+          add_row key: 'value'
+        end
+      end
+
+      Twine::Runner.new(nil, options, @twine_file)
+    end
+
+    def test_fails_if_output_folder_does_not_exist
+      assert_raises Twine::Error do
+        new_runner(false).generate_all_string_files
+      end
+    end
+
+    def test_creates_output_folder
+      FileUtils.remove_entry_secure @output_dir
+      new_runner(true).generate_all_string_files
+      assert File.exists? @output_dir
+    end
+
+    def test_does_not_create_language_folders_by_default
+      Dir.mkdir File.join @output_dir, 'en.lproj'
+      new_runner(false).generate_all_string_files
+      refute File.exists?(File.join(@output_dir, 'es.lproj')), "language folder should not be created"
+    end
+
+    def test_creates_language_folders
+      new_runner(true).generate_all_string_files
+      assert File.exists?(File.join(@output_dir, 'en.lproj')), "language folder 'en.lproj' should be created"
+      assert File.exists?(File.join(@output_dir, 'es.lproj')), "language folder 'es.lproj' should be created"
+    end
+  end
+end
