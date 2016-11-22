ee5b5c96a8b1b06761ca7a1fce504ecffb504f28
Sebastian Ludwig <sebastian@lurado.de>
2016-11-22 22:27:02 +0100

    Implemented can_handle_directory? and determine_language_given_path for Flash formatter.

--- lib/twine/formatters/flash.rb
+++ lib/twine/formatters/flash.rb
@@ -11,16 +11,14 @@ module Twine
         '.properties'
       end
 
-      def can_handle_directory?(path)
-        return false
-      end
-
       def default_file_name
         'resources.properties'
       end
 
       def determine_language_given_path(path)
-        return
+        # match two-letter language code, optionally followed by a two letter region code
+        path.split(File::SEPARATOR).reverse.find { |segment| segment =~ /^([a-z]{2}(-[a-z]{2})?)$/i }
+      end
 
       def set_translation_for_key(key, lang, value)
         value = convert_placeholders_from_android_to_twine(value)
