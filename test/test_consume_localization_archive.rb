d0dc544023e8a1abb25204234d7c40ef47198734
Sebastian Ludwig <sebastian@lurado.de>
2018-05-21 13:43:57 +0200

    Let consume-localization-archive fail if one file could not be consumed. Fixes #243.

--- test/test_consume_localization_archive.rb
+++ test/test_consume_localization_archive.rb
@@ -4,24 +4,30 @@ class TestConsumeLocalizationArchive < CommandTest
   def setup
     super
 
-    options = {}
-    options[:input_path] = fixture_path 'consume_localization_archive.zip'
-    options[:output_path] = @output_path
-    options[:format] = 'apple'
-
     @twine_file = build_twine_file 'en', 'es' do
       add_section 'Section' do
         add_definition key1: 'value1'
       end
     end
+  end
 
-    @runner = Twine::Runner.new(options, @twine_file)
+  def new_runner(options = {})
+    options[:input_path] = fixture_path 'consume_localization_archive.zip'
+    options[:output_path] = @output_path
+
+    Twine::Runner.new(options, @twine_file)
   end
 
   def test_consumes_zip_file
-    @runner.consume_localization_archive
+    new_runner(format: 'android').consume_localization_archive
 
     assert @twine_file.definitions_by_key['key1'].translations['en'], 'value1-english'
     assert @twine_file.definitions_by_key['key1'].translations['es'], 'value1-spanish'
+  end
+
+  def test_raises_error_if_format_ambiguous
+    assert_raises Twine::Error do
+      new_runner.consume_localization_archive
+    end
   end
 end
